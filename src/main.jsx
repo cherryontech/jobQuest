b867b76be11326bcd3ce0a8786cc35f744e14eef
Pms-crypto <poorvashukla315@gmail.com>
2023-11-03 20:50:56 +0530

    fix: folder named screens is renamed to pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { LandingPage } from './screens/LandingPage'
+import { LandingPage } from './pages/LandingPage'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
