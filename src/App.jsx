8ab78b3dbadab1a2e4a98a8a9234e5d46fb2e1d5
Pms-crypto <poorvashukla315@gmail.com>
2023-11-18 18:14:56 +0530

    feat: Roadmap page upper half structure + styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
 import { LoginPage } from "./pages/Login";
+import { RoadmapPage } from "./pages/RoadmapPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <>
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/roadmap" element={<RoadmapPage />} />
           </>
         </Routes>
       </Router>
--- src/pages/RoadmapPage/RoadmapPage.jsx
+++ src/pages/RoadmapPage/RoadmapPage.jsx
@@ -1,112 +1,97 @@
 import { Buttons } from "../../components/Buttons";
 import "./style.css";
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 export const RoadmapPage = () => {
   return (
-    <div>
-      <Tabs>
-        <TabList>
-          <Tab>Free Flowing</Tab>
-          <Tab>Linear Path</Tab>
-        </TabList>
-        <TabPanel>
-                  <img
-                    className="line"
-                    alt="Line"
-                    src="src/assets/Line.png"
-                  />
-                  <p className="p">
+    <div className="index">
+      <p className="advice-tips">Advice, Tips &amp; Actionable Steps</p>
+      <p className="text-wrapper-5">
+        All you need for job search success in one place!
+      </p>
+      <div className="top-nav-bar">
+        <div className="top-nav-bar-2">
+          <div className="text-wrapper-6">Job Quest</div>
+        </div>
+      </div>
+
+      <img className="img" src="https://via.placeholder.com/634x552" />
+      {/* <div className="text-wrapper-4">Choose your learning style:</div> */}
+      <div className="ellipse" />
+      <div className="overlap">
+        <div className="page-BG" />
+        <div className="bottom-card-section">
+          <div className="overlap-group">
+            <div className="free-flowing">
+              <Tabs className="tab text-wrapper-2">
+                <TabList>
+                  <Tab className="tab-title">Free Flowing</Tab>
+                  <Tab className="tab-title">Linear Path</Tab>
+                </TabList>
+                <TabPanel>
+                  {/* <FreeFlow /> */}
+                  <img alt="Line" src="src/assets/Line.png" />
+                  {/* <p className="p linear-path overlap-group-2 text-wrapper-2"> */}
+                  <p className="text-wrapper-3">
                     Banish overwhelm. Complete each task one step at a time to
                     unlock the next step.
                   </p>
-                  </TabPanel>
-                  <TabPanel>
+                </TabPanel>
+                <TabPanel>
+                  {/* <LinearPath/> */}
+                  <img alt="Line" src="src/assets/Line.png" />
+                  {/* <p className="text-wrapper-3"> */}
+                  <p className="text-wrapper-3">
+                    Dip in and out and complete whatever tasks you like whenever
+                    you want!
+                  </p>
+                </TabPanel>
+              </Tabs>
+            </div>
+            {/* <div className="linear-path">
+                <div className="overlap-group-2">
+                  <div className="text-wrapper">Linear Path</div>
+                  <img
+                    className="line"
+                    alt="Line"
+                    src="https://cdn.animaapp.com/projects/652ee00b2873525a7a887798/releases/654f0413f197b5633923a301/img/line-1-1.svg"
+                  />
+                  <p className="p">Banish overwhelm. Complete each task one step at a time to unlock the next step.</p>
+                </div>
+              </div> */}
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group-3">
+                <div className="overlap-group-wrapper">
+                  <div className="overlap-group-3">
+                    <div className="rectangle" />
+                    <div className="rectangle-2" />
+                  </div>
+                </div>
+
+                {/* <div className="free-flowing">
+                    <div className="text-wrapper-2">Free Flowing</div>
                     <img
                       className="img"
                       alt="Line"
-                      src="src/assets/Line.png"
+                      src="https://cdn.animaapp.com/projects/652ee00b2873525a7a887798/releases/654f0413f197b5633923a301/img/line-2.svg"
                     />
                     <p className="text-wrapper-3">
-                      Dip in and out and complete whatever tasks you like
-                      whenever you want!
+                      Dip in and out and complete whatever tasks you like whenever you want!
                     </p>
-                    </TabPanel>
-      </Tabs>
-      <img
-            className="JQ-final-colors"
-            alt="Jq final colors"
-            src="src/assets/RoadmapMascot.png"
-          />
-          <div className="text-wrapper-4">Choose your learning style:</div>
-          <div className="ellipse" />
-          <div className="cards">
-            <div className="cards-2">
-              <div className="top-row">
-              </div>
-              <div className="second-row">
+                  </div> */}
               </div>
             </div>
           </div>
-        <p className="advice-tips">Advice, Tips &amp; Actionable Steps</p>
-        <p className="text-wrapper-5">
-          All you need for job search success in one place!
-        </p>
-        <div className="top-nav-bar">
-          <div className="top-nav-bar-2">
-            <div className="text-wrapper-6">Job Quest</div>
-            <div className="buttons-for-sign-up">
-              <div className="buttons-mini-2" />
-              {/* <ButtonsMini
-                buttonsLineMini="Log Out"
-                buttonsLineMiniClassName="design-component-instance-node"
-                buttonsLineMiniClassNameOverride="buttons-mini-3"
-                className="buttons-mini-instance"
-                property1="outlined"
-              /> */}
-                <Buttons buttonsSolidLargeText="Log out" className="buttons-instance" property1="solid-large-blue" />
-
-            </div>
-          </div>
         </div>
-        <div className="frame">
-          <img
-            className="image"
-            alt="Image"
-            src="src/assets/RoadmapImage.png"
-          />
-        </div>
+        <img
+          className="JQ-final-colors"
+          alt="Jq final colors"
+          src="https://cdn.animaapp.com/projects/652ee00b2873525a7a887798/releases/654f0413f197b5633923a301/img/jq-final-colors-1@2x.png"
+        />
+        <div className="text-wrapper-4">Choose your learning style:</div>
+        <div className="ellipse" />
       </div>
-    // <div className="index">
-    //   <div className="div">
-    //     <div className="overlap">
-    //       <div className="page-BG" />
-    //       <div className="bottom-card-section">
-    //         <div className="overlap-group">
-    //           <div className="linear-path">
-    //             <div className="overlap-group-2">
-    //               <div className="text-wrapper">
-    //               </div>
-                  
-    //             </div>
-    //           </div>
-    //           <div className="overlap-group-wrapper">
-    //             <div className="overlap-group-3">
-    //               <div className="overlap-group-wrapper">
-    //                 <div className="overlap-group-3">
-    //                   <div className="rectangle" />
-    //                   <div className="rectangle-2" />
-    //                 </div>
-    //               </div>
-    //               <div className="free-flowing">
-    //                 {/* <div className="text-wrapper-2">Free Flowing</div> */}
-                    
-    //               </div>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-         
+    </div>
   );
 };
