e0cfff2372e0ee8aabbf9e396a05fd41216333da
Pms-crypto <poorvashukla315@gmail.com>
2023-11-24 18:11:39 +0530

    fix: removed repetitive cursor:pointer css style and added tailwind classname to the components

--- src/components/Buttons/index.jsx
+++ src/components/Buttons/index.jsx
@@ -4,20 +4,22 @@ import "./style.css";
 
 export const Buttons = ({ property1, extraClasses, buttonsSolidLargeText = "Start Free"}) => {
   return (
-    <div className={`buttons ${extraClasses}`}>
-      <ButtonsSolidLarge
-        extraClasses={`${
-          property1 === "hover"
+    <div className="cursor-pointer">
+      <div className={`buttons ${extraClasses}`}>
+        <ButtonsSolidLarge
+          extraClasses={`${
+            property1 === "hover"
             ? "class-4"
             : property1 === "outline"
             ? "class-5"
             : property1 === "outlined-hover"
             ? "class-6"
             : "class-7"
-        }`}
+          }`}
         divClasses={`${property1 === "outline" ? "class" : property1 === "outlined-hover" ? "class-2" : "class-3"}`}
         text={buttonsSolidLargeText}
       />
+      </div>
     </div>
   );
 };
