aee2103efcb8d74ccb179298c9d59186734f8b88
khairahscorner <airahyusuff@gmail.com>
2023-11-25 03:05:57 +0000

    update free-flowing card to pass parameter

--- src/components/FreeflowingCard/index.jsx
+++ src/components/FreeflowingCard/index.jsx
@@ -1,26 +1,33 @@
 import PropTypes from "prop-types";
-import {
-    Card,
-    CardBody,
-} from "@nextui-org/react";
+import { Card, CardBody } from "@nextui-org/react";
+import { useNavigate } from "react-router-dom";
 import { PercentageScore } from "../PercentageScore";
 import "./style.css";
 
-export const FreeflowingCard = ({cardName}) => {
-    const currentCardName = cardName;
+export const FreeflowingCard = ({ cardName }) => {
+  const navigateTo = useNavigate();
 
-    return (
-        <Card className="card-div">
-                  <CardBody>
-                    {/* <div className="card-div"> */}
-                    <PercentageScore />
-                <p className="card-name">{currentCardName}</p>
-                    {/* </div> */}
-                  </CardBody>
-                </Card>
-    )
-}
+  const currentCardName = cardName;
+
+  const checkIfLoggedIn = () => {
+    if (localStorage.getItem("jobQuestEmail")) {
+      navigateTo(`/${currentCardName}`); //navigate to taskCard
+    } else {
+      //to-do: show user alert that they need to login
+      navigateTo("/login");
+    }
+  };
+
+  return (
+    <Card className="card-div cursor-pointer" onClick={() => checkIfLoggedIn()}>
+      <CardBody>
+        <PercentageScore />
+        <p className="card-name">{currentCardName}</p>
+      </CardBody>
+    </Card>
+  );
+};
 
 FreeflowingCard.propTypes = {
-    cardName: PropTypes.string
-  };
+  cardName: PropTypes.string,
+};
