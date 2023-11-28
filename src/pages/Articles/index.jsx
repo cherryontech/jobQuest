9f9831e56639c43ce95074055ac99e5f90bff196
T-Orisadare <pndebele@wesleyan.edu>
2023-11-27 19:28:09 -0500

    populated static article information

--- src/pages/Articles/index.jsx
+++ src/pages/Articles/index.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image } from "@nextui-org/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Divider,
+  Image,
+  ScrollShadow,
+} from "@nextui-org/react";
+import articles from './articles.json'
+
 export default function Articles() {
   const navigateTo = useNavigate();
 
   return (
-    <div className="p-10 h-screen" style={{ backgroundColor: "#D9E2F3" }}>
-      <div className="w-4/5 m-auto px-20">
+    <div className="p-10 " style={{ backgroundColor: "#D9E2F3" }}>
+      <div className="w-4/6 m-auto ">
         <p className=" text-gray opacity-50 mb-10">
           <span
             className="hover:text-[#FF6667] hover:opacity-100 hover:cursor-pointer"
@@ -39,27 +48,38 @@ export default function Articles() {
       </div>
 
       <div
-        className="w-5/6 m-auto bg-white p-2 flex wrap-2"
+        className="w-4/6 m-auto bg-white p-12"
         style={{ borderRadius: "30px" }}
       >
-        <Card className="max-w-[400px]">
-          {/* <CardHeader className=""> */}
-            <Image
-              alt="nextui logo"
-            //   height={40}
-            //   radius="sm"
-              src="https://inspirationseek.com/wp-content/uploads/2016/02/Cute-Dog-Photo.jpg"
-            //   width={40}
-            // className="obje"
-            style={{objectFit: 'fill'}}
-            />
-           
-          {/* </CardHeader> */}
-          <Divider />
-          <CardBody>
-            <p>Make beautiful websites regardless of your design experience.</p>
-          </CardBody>
-        </Card>
+        <h1 className="text-4xl font-bold mb-1 text-[#25274D] mb-8">
+          LinkedIn Profile
+        </h1>
+        <div className="flex flex-wrap justify-center ">
+          {/* TODO: 'ADD COLORED SCROLL BAR' */}
+          {/* <ScrollShadow> */}
+          {articles.map((article, index) => (
+            <Card key={index} className="w-[360px] max-w-[400px] m-5">
+              <CardHeader className="m-0 max-h-[250px] p-0">
+                <Image radius="none" src={article.image} />
+              </CardHeader>
+              <Divider />
+              <CardBody>
+                <p className="text-2xl font-bold text-[#25274C]">
+                  {article.title}
+                </p>
+                <span className="text-[#78858F]">{article.writer}</span>
+                <br />
+                <p className="text-[#25274C]">
+                  {article.quote}{" "}
+                  <a href={article.link} className="text-danger cursor-pointer">
+                    {article.clickable}
+                  </a>
+                </p>
+              </CardBody>
+            </Card>
+          ))}
+          {/* </ScrollShadow> */}
+        </div>
       </div>
     </div>
   );
