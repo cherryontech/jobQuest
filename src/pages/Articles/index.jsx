0d245972299baa5babeb398a04b72a88267f8806
Pms-crypto <poorvashukla315@gmail.com>
2023-12-06 11:56:56 +0530

    fix: update styling to see 2 articles per row in my screen

--- src/pages/Articles/index.jsx
+++ src/pages/Articles/index.jsx
@@ -56,7 +56,7 @@ export default function Articles() {
       </div>
 
       <div
-        className="w-4/6 m-auto bg-white p-12"
+        className="w-4/6 m-auto bg-white p-10"
         style={{ borderRadius: "30px" }}
       >
         <h1 className="text-4xl font-bold text-[#25274D] my-8 ml-7">
