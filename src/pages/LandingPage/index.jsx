5896fd861b3797cd360e85580ccd6495be8eaa07
beatsnmelody <beatsnmelody1@gmail.com>
2023-12-05 21:02:33 -0600

    started footer

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,29 +1,51 @@
 import { Buttons } from "../../components/Buttons";
 import "./style.css";
-import finalHomepage from "../../assets/finalHomepage.png"
+import finalHomepage from "../../assets/finalHomepage.png";
 import { useNavigate } from "react-router-dom";
 
 export const LandingPage = () => {
-  const navigateTo = useNavigate()
+  const navigateTo = useNavigate();
 
   return (
-    <div className="landing-page">
-      <div className="div">
-        <div className="overlap">
-          <div className="landing-section">
-            <img className="image" alt="Image" src={finalHomepage} />
-            <div className="overlap-group">
-              <p className="p">Join for free in order to save your progress and keep track of your tasks.</p>
+    <div className="homepage">
+      <div className="landing-page">
+        <div className="div">
+          <div className="overlap">
+            <div className="landing-section">
+              <img className="image" alt="Image" src={finalHomepage} />
+              <div className="overlap-group">
+                <p className="p">
+                  Join for free in order to save your progress and keep track of
+                  your tasks.
+                </p>
+              </div>
+              <p className="text-wrapper-2">
+                Your path to employment: streamlined job search guidance
+              </p>
+              <p className="text-wrapper-3">
+                Feeling overwhelmed with job searching?
+              </p>
             </div>
-            <p className="text-wrapper-2">Your path to employment: streamlined job search guidance</p>
-            <p className="text-wrapper-3">Feeling overwhelmed with job searching?</p>
+            <Buttons
+              clickHandler={() => navigateTo("/roadmap")}
+              buttonsSolidLargeText="Start Now"
+              extraClasses="buttons-instance"
+              property1="solid-large-blue"
+            />
+            <footer className="footer">
+              <div className="info">
+                <div className="menu-footer">
+                  <p className="menu-footer-instance">For Job Seekers</p>
+                  <p className="menu-footer-vert-instance">
+                    Career Advice 
+                    Job Search Guidance 
+                    Your Job Profile 
+                    Help
+                  </p>
+                </div>
+              </div>
+            </footer>
           </div>
-          <Buttons
-            clickHandler={()=> navigateTo('/roadmap')}
-            buttonsSolidLargeText="Start Now"
-            extraClasses="buttons-instance"
-            property1="solid-large-blue"
-          />
         </div>
       </div>
     </div>
