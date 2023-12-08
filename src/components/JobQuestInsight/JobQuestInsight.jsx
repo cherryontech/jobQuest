89ffb6a42ab73ac9710914ed3a6f168753c0cb46
T-Orisadare <pndebele@wesleyan.edu>
2023-12-08 17:50:39 -0500

    updating styling

--- src/components/JobQuestInsight/JobQuestInsight.jsx
+++ src/components/JobQuestInsight/JobQuestInsight.jsx
@@ -1,56 +1,61 @@
-import './insight.css'
+import "./insight.css";
 import arrows from "../../assets/arrows.svg";
 import toolkit from "../../assets/toolkit.svg";
 import checkmark from "../../assets/checkmark.svg";
 
-export default function JobQuestInsight () {
-    return (
-        <div className="absolute top-[880px] flex items-center justify-center h-[600px] w-[100%] insight">
-        <div className="absolute w-4/6 ">
-          <h1 className="text-5xl text-[#2C2C2C] font-bold my-8  text-center">
-            Wondering whether Job Quest is for you?
-          </h1>
-          <div className="flex justify-around">
-            <div className="flex flex-col p-6 ">
-              <img className="w-1/3 h-1/3 m-auto" src={arrows} alt="arrows" />
-              <p className="text-[#2C2C2C] text-2xl font-bold my-6">
-                Two paths for all users
-              </p>
-              <span className="text-[#4E4E4E] spanFont w-[100%]">
-                Users can choose between a linear structured path and a free
-                flowing path.
-              </span>
-            </div>
-            <div className="flex flex-col p-6 ">
-              <img
-                className="w-1/3 h-1/3 m-auto"
-                src={toolkit}
-                alt="arrows"
-              />
-              <p className="text-[#2C2C2C] text-2xl font-bold my-6 ">
-                Centralized job guidance
-              </p>
-              <span className="text-[#4E4E4E] spanFont  w-[100%]">
-                A hub that offers tools for resume-building, interview
-                preparation, and career advice.
-              </span>
-            </div>
-            <div className="flex flex-col p-6 ">
-              <img
-                className="w-1/3 h-1/3 m-auto"
-                src={checkmark}
-                alt="arrows"
-              />
-              <p className="text-[#2C2C2C] text-2xl font-bold my-6">
-                Organized job search
-              </p>
-              <span className="text-[#4E4E4E] spanFont w-[100%] ">
-                Task management feature such as tick lists to keep job search
-                organized.
-              </span>
-            </div>
+export default function JobQuestInsight() {
+  return (
+    <div className="absolute top-[880px] flex items-center justify-center h-[600px] w-[100%] insight">
+      <div className="absolute w-4/6 ">
+        <h1 className="text-5xl text-[#2C2C2C] font-bold my-8  text-center">
+          Wondering whether Job Quest is for you?
+        </h1>
+        <div className="flex justify-between">
+          <div className="flex flex-col w-1/3 p-6 h-[310px] max-h-100 ">
+            <img
+              className="w-15 h-15 ml-auto mr-auto"
+              src={arrows}
+              alt="arrows"
+            />
+            <p className="text-[#2C2C2C] text-center text-2xl font-bold mb-6 mt-10">
+              Two paths for all users
+            </p>
+            <span className="text-[#4E4E4E] spanFont">
+              Users can choose between a linear structured path and a free
+              flowing path.
+            </span>
+          </div>
+          <div className="flex flex-col w-2/5 p-6 h-[310px] max-h-100 ">
+            <img
+              className="w-15 h-15 ml-auto mr-auto"
+              src={toolkit}
+              alt="arrows"
+            />
+            <p className="text-[#2C2C2C] text-center text-2xl font-bold mb-6 mt-10">
+              Centralized job guidance
+            </p>
+            <span className="text-[#4E4E4E] spanFont">
+              A hub that offers tools for <br /> resume-building, interview
+              preparation, <br />
+              and career advice.
+            </span>
+          </div>
+          <div className="flex flex-col w-1/3 p-6 h-[310px] max-h-100 ">
+            <img
+              className="w-15 h-15 ml-auto mr-auto"
+              src={checkmark}
+              alt="arrows"
+            />
+            <p className="text-[#2C2C2C] text-center text-2xl font-bold  mb-6 mt-10">
+              Organized job search
+            </p>
+            <span className="text-[#4E4E4E] spanFont">
+              Task management feature such as tick lists to keep job search
+              organized.
+            </span>
           </div>
         </div>
       </div>
-    )
+    </div>
+  );
 }
